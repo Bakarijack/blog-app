89e33a40cb0e400abecf5e3e919ab438c3368c28
Bakarijack <bakarikilu254@gmail.com>
2023-11-14 19:05:18 +0300

    toggle menu feature creation started

--- src/components/header/mini_components/Menu.js
+++ src/components/header/mini_components/Menu.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import ToggleBtn from './ToggleBtn'
 
 const Menu = () => {
   return (
-    <nav>Menu</nav>
+    <nav>
+      <ToggleBtn/>
+    </nav>
   )
 }
 
---
+++ src/components/header/mini_components/ToggleBtn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToggleBtn = () => {
+  return (
+    <button className='menu-toggle'>toggle menu</button>
+  )
+}
+
+export default ToggleBtn
